ed4afee972325bc6a6369e85e40964817be9fc00
xiaolv <lvzhiping1994@163.com>
2023-03-23 23:14:19 +0800

    0.0

---
+++ service-oa/src/main/java/com/atguigu/process/mapper/OaProcessTypeMapper.java
@@ -0,0 +1,16 @@
+package com.atguigu.process.mapper;
+
+import com.atguigu.model.process.ProcessType;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 审批类型 Mapper 接口
+ * </p>
+ *
+ * @author xiaolv
+ * @since 2023-03-22
+ */
+public interface OaProcessTypeMapper extends BaseMapper<ProcessType> {
+
+}
